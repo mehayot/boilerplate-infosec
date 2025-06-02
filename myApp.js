bcff3bd1356f1b406cd9711f17b6e2927700e36d
haytboy <84987186+mehayot@users.noreply.github.com>
2025-06-02 20:27:24 +0900

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ const app = express(); // Bu qator kerak
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter({}));
-app.use(helmet.noSniff({}))
+app.use(helmet.noSniff());
 
 
 
