7892367dccc755afe1d74f0aa9e2474008f5c84f
haytboy <84987186+mehayot@users.noreply.github.com>
2025-06-04 00:24:26 +0900

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const helmet = require('helmet');
-const app = express(); // Bu qator kerak
-var ninetyDaysInSeconds = 90*24*60*60;
+const app = express();
+
+const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
 
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({action: 'deny'}));
-app.use(helmet.xssFilter({}));
+app.use(helmet.frameguard({ action: 'deny' }));
+app.use(helmet.xssFilter());
 
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
 
 
 
