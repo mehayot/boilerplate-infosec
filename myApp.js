7b2906f56fc4939359c92c619306d7936f19d2b6
haytboy <84987186+mehayot@users.noreply.github.com>
2025-06-04 01:48:18 +0900

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,15 +2,21 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
-const ninetyDaysInSeconds = 90 * 24 * 60 * 60;
-
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
-
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
-app.use(helmet.hsts({ maxAge: ninetyDaysInSeconds, force: true }));
+
+var  ninetyDaysInSeconds = 90 * 24 * 60 * 60;
+app.use(
+  helmet.hsts({
+    maxAge: 7776000, // 90 kun sekundda (90*24*60*60)
+    includeSubDomains: true, // ixtiyoriy
+    preload: true, // ixtiyoriy
+  })
+);
+
 
 
 
